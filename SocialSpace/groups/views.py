088f782e26172fce1ddada5dba325c50889ce9ae
RishabhShekhar <rishabhshekhar04@gmail.com>
2020-10-30 23:46:55 +0530

    From atom to PyCharm

--- SocialSpace/groups/views.py
+++ SocialSpace/groups/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.views import generic
 from groups.models import Group,GroupMember
 # Create your views here.
