1fb4a263827e75b3c7365054df47013b1c975364
RishabhShekhar <rishabhshekhar04@gmail.com>
2020-11-07 14:52:50 +0530

    LeaveGroup View

--- SocialSpace/groups/views.py
+++ SocialSpace/groups/views.py
@@ -7,6 +7,7 @@ from groups.models import Group, GroupMember
 from django.shortcuts import get_object_or_404
 from django.db import IntegrityError
 from django.contrib import messages
+from . import models
 
 
 # Create your views here.
@@ -40,3 +41,24 @@ class JoinGroup(LoginRequiredMixin, generic.RedirectView):
             messages.success(self.request, 'You are now a member!')
 
         return super().get(request, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin,generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
+
+    def get(self,request,*args,**kwargs):
+
+        try:
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get('slug')
+            ).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, 'Sorry you are not in this group!')
+        else:
+            membership.delete()
+            messages.success(self.request, 'You have left the group!')
+        return super().get(request, *args, **kwargs)
+
