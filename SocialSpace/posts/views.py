05ce2f9442053a5b62a6fb945ceda24e9f7c0121
RishabhShekhar <rishabhshekhar04@gmail.com>
2020-12-02 00:18:48 +0530

    post_user

--- SocialSpace/posts/views.py
+++ SocialSpace/posts/views.py
@@ -23,11 +23,11 @@ class UserPosts(generic.ListView):
 
     def get_queryset(self):
         try:
-            self.post.user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+            self.post_user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
         except User.DoesNotExist:
             raise Http404
         else:
-            return self.posts.all()
+            return self.post_user.posts.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
